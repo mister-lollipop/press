b8467e96f02d55e70b93a7505d90a07016143d1c
mister-lollipop <kolliparasudhakar850@gmail.com>
2024-10-18 11:47:27 +0530

    Update press.tf

--- press.tf
+++ press.tf
@@ -10,7 +10,7 @@ variable "aws_secret_key" {
 provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
-  region     = "us-east-1" 
+  region     = "us-west-1" 
 }
 resource "aws_instance" "instance" {
   ami                         = "ami-0175bdd48fdb0973b"
