8828947b1a054e6f747192ec713c3d972a729d8c
mister-lollipop <kolliparasudhakar850@gmail.com>
2024-10-18 10:07:12 +0530

    Add files via upload

---
+++ press.tf
@@ -0,0 +1,59 @@
+variable "aws_access_key" {
+  description = "AWS Access Key"
+  type        = string
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Key"
+  type        = string
+}
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = "us-east-1" 
+}
+resource "aws_instance" "instance" {
+  ami                         = "ami-00f251754ac5da7f0"
+  instance_type               = "t2.medium"
+  key_name                    = "yash"
+  vpc_security_group_ids      = [aws_security_group.securitygroup1.id]
+  associate_public_ip_address = true
+  user_data                   = file("data.sh")
+  tags = {
+    Name = "instance"
+  }
+}
+
+#creating security group
+
+resource "aws_security_group" "securitygroup1" {
+
+  #inbound Rules
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "securitygroup1"
+  }
+}
